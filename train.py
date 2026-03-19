fe8e8f1cfb7608b7e208114743e37d3f1e3e90cb
eddableheath <edable-heath@turing.ac.uk>
2026-03-19 08:42:54 +0000

    prints training type

--- train.py
+++ train.py
@@ -36,6 +36,8 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    print(f"Training type: {args.training_type}")
+
     if args.training_type == "full_reference_ast":
         FullReferenceWithASTTraining(
             csv_path=args.csv_path,
